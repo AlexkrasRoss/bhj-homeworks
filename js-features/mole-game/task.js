8554c1c14b5293638025312889a2fcd22a063681
Alex hitsaaja <sto98@inbox.ru>
2024-02-06 23:18:18 +0200

    Initial commit task.js

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+let counterDead = 0;
+let counterLost = 0;
+getHole = index => document.getElementById(`hole${index}`);
+for (let index = 1; index < 9; index++) {
+    let hole = getHole(index);
+    hole.onclick = function() {
+        if (hole.classList.contains('hole_has-mole')) {
+            counterDead++;
+            dead.textContent = counterDead;
+        } else {
+            counterLost++;
+            lost.textContent = counterLost;
+        }
+        if (counterDead === 10) {
+            alert('Вы выйграли');
+            counterDead = 0;
+            counterLost = 0;
+            dead.textContent = counterDead;
+            lost.textContent = counterLost;
+        }
+        if (counterLost === 5) {
+            alert('Вы проиграли');
+            counterDead = 0;
+            counterLost = 0;
+            lost.textContent = counterLost;
+            dead.textContent = counterDead;
+        }
+    };
+
+}
