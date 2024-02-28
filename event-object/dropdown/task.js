26e8da4cfe2b9c0c0db354ac0f43f2e5b8b5ae2a
Alex hitsaaja <sto98@inbox.ru>
2024-02-28 22:09:54 +0200

    commit 2802242209 task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,15 +1,15 @@
 const dropdownValue = document.querySelector('.dropdown__value'),
 	dropdownItem = (document.querySelectorAll('.dropdown__item'));
 
-dropdownValue.addEventListener('click', myFunction);
+dropdownValue.addEventListener('click', dropdownList);
 
-function myFunction() {
+function dropdownList() {
 	document.querySelector('.dropdown__list').classList.toggle('dropdown__list_active');
 }
 
 dropdownItem.forEach((element, index) => {
 	element.onclick = (event) => {
-		myFunction();
+		dropdownList();
 		dropdownValue.textContent = dropdownItem[index].textContent;
 		event.preventDefault();
 	}
