268b26f6dd390dcadfb7a2cce9c055315edbf7c2
Alex hitsaaja <sto98@inbox.ru>
2024-04-23 12:08:42 +0300

    commit 2304241208 task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,87 +1,43 @@
-class Counter {
-	constructor({
-		selector,
-		initCount = 0,
-		step = 1
-	}) {
-		const parent = document.querySelector(selector);
-		this.count = initCount;
-		this.step = step;
+let cartWrapper = document.querySelector('.cart__products');
+// СЧЕТЧИК 
+window.addEventListener("click", e => {
+	const target = e.target
+	if (target.classList.contains('product__quantity-control_inc') || target.classList.contains('product__quantity-control_dec')) {
+		const counterWrapper = target.closest('.product__quantity-controls')
+		const counter = counterWrapper.querySelector('.product__quantity-value')
+		if (target.classList.contains('product__quantity-control_inc')) {
+			counter.innerText = ++counter.innerText
+		}
+		if (target.classList.contains('product__quantity-control_dec')) {
+			if (counter.innerText > 1) {
+				counter.innerText = --counter.innerText
+			}
+		}
+	}
+})
 
-		this.refs = {
-			buttonDecrement: parent.querySelector('.product__quantity-control_dec'),
-			buttonIncrement: parent.querySelector('.product__quantity-control_inc'),
-			spanCount: parent.querySelector('.product__quantity-value'),
-			addBasket: parent.querySelector('.product__add'), 
-		};
-		this.addListeners();
-		this.updateCount();
-	}
-	updateCount() {
-		this.refs.spanCount.textContent = this.count;
-	}
+// ДОБАВЛЯЕМ ТОВАР В КОРЗИНУ
+window.addEventListener("click", function(event) {
+	if (event.target.classList.contains('product__add')) {
+		const card = event.target.closest(".product");
+		const productInfo = {
+			id: card.dataset.id,
+			imgSrc: card.querySelector('.product__image').getAttribute('src'),
+			counter: card.querySelector('.product__quantity-value').innerText,
+		}
 
-	addListeners() {
-		this.refs.buttonDecrement.addEventListener("click", this.onDecrement.bind(this), );
-		this.refs.buttonIncrement.addEventListener("click", this.inDecrement.bind(this), );
-		this.refs.addBasket.addEventListener("click", this.addToBasket.bind(this),);
+		// ТОВАР НЕ ДУБЛИРОВАЛСЯ В КОРЗИНЕ
+		const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+		if (itemInCart) {
+			const counterElement = itemInCart.querySelector('.cart__product-count');
+			counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+		} else {
+			const cartItemHTML = `<div class="cart__product" data-id=${productInfo.id}>
+                <img class="cart__product-image" src="${productInfo.imgSrc}">
+                <div class="cart__product-count">${productInfo.counter}</div>
+            </div>`;
+			cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+		}
+		card.querySelector('.product__quantity-value').innerText = '1';
 	}
-	onDecrement() {
-		if (this.count < 2) return;
-		this.count -= this.step;
-		this.updateCount();
-	}
-
-	inDecrement() {
-		this.count += this.step;
-		this.updateCount();
-	}
-	addToBasket() {
-
-	}
-}
-new Counter({
-	selector: '[data-id="1"]',
-	initCount: 1,
-	step: 1
 });
-new Counter({
-	selector: '[data-id="2"]',
-	initCount: 1,
-	step: 1
-});
-new Counter({
-	selector: '[data-id="3"]',
-	initCount: 1,
-	step: 1
-});
-new Counter({
-	selector: '[data-id="4"]',
-	initCount: 1,
-	step: 1
-});
-
-let cartProducts = document.querySelector('.cart__products');
-let add = document.querySelector('.product__add');
-let cart = document.querySelector('.cart');
-
-// разметка добавленного в корзину товара
-const elementDiv = document.createElement('div');
-const elementImg = document.createElement('img');
-const elementDiv2 = document.createElement('div');
-
-elementDiv.classList.add('cart__product');
-cartProducts.append(elementDiv);
-elementDiv.setAttribute('data-id','1')
-elementDiv.append(elementImg);
-elementImg.classList.add('cart__product-image');
-elementImg.setAttribute('src','image.png')
-elementDiv.append(elementDiv2);
-elementDiv2.classList.add('cart__product-count');
-elementDiv2.textContent='20'
-
-add.addEventListener("click",e=>{
-  e.preventDefault();
-  let target = e.target;
-elementImg.src="https://pokushai.kz/uploads/files/640x480_f8f13b9f33dbb71ced4a24d4a9e09c89.jpg";
-  })
