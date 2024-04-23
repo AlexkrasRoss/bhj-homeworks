3743dccc26b106c60443571dddf000ec6683beee
Alex hitsaaja <sto98@inbox.ru>
2024-04-23 12:07:09 +0300

    commit 2304241205 task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,32 +1,31 @@
-const input = document.querySelector("#task__input");
-const btn = document.querySelector("#tasks__add");
+let input = document.querySelector("#task__input");
+let btn = document.querySelector("#tasks__add");
 let result = document.querySelector("#tasks__list");
-const body = document.querySelector('main')
 
+// кнопка добавить новую задачу
 btn.addEventListener("click", (e) => {
 	e.preventDefault();
-	createElements(input.value)
-	input.value = ''
+	createElements(input.value);
+	input.value = ''.trim();
 })
 
+// поле новая задача
 function createElements(value) {
-	if (input.value === '') return
-	let divTask = document.createElement('div');
-	let divTaskA = document.createElement('a');
-	divTask.classList.add('task');
-	divTaskA.setAttribute('href', '#');
-	divTask.classList.add('task__title');
-	divTaskA.classList.add('task__remove', );
-
-	result.appendChild(divTask);
-	result.appendChild(divTaskA);
-
-	divTask.textContent = value
-	divTask.appendChild(divTaskA);
-	divTaskA.innerHTML = '&times;';
-
-	divTaskA.addEventListener("click", (e) => {
+	if (input.value === ''.trim()) return;
+	let divTag = `<div class="task">
+                      <div class="task__title">
+                        ${input.value}
+                      </div>
+                      <a href="#" class="task__remove">&times;</a>
+                    </div>`
+	result.insertAdjacentHTML('beforeend', divTag);
+	
+// удаление поля новая задача
+	window.addEventListener("click", e => {
 		e.preventDefault();
-		divTask.remove();
+		if (e.target.classList.contains('task__remove')) {
+			const card = e.target.closest(".task");
+			card.remove();
+		};
 	});
 };
