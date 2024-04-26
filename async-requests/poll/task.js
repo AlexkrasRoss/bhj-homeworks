9e337b2a29c6505fe048f8ecf3f044180ceafd53
Alex hitsaaja <sto98@inbox.ru>
2024-04-26 16:19:58 +0300

    Initial commit task.js

---
+++ async-requests/poll/task.js
@@ -0,0 +1,37 @@
+const reURL = 'https://students.netoservices.ru/nestjs-backend/poll'
+const recuestURL = 'https://jsonplaceholder.typicode.com/users'
+const xhr = new XMLHttpRequest();
+
+// // Асинхронный метод
+xhr.open('GET', reURL);
+xhr.responseType = 'json';
+let answer = xhr.response;
+xhr.onload = () => {
+	if (xhr.status >= 400) {
+		console.error(xhr.response)
+	} else {
+		let pTitle = document.querySelector(".poll__title");
+		let title = `${xhr.response.data.title}`
+		pTitle.insertAdjacentHTML('beforeend', title);
+
+		let pollAnsers = xhr.response.data.answers
+		pollAnsers.forEach((item, index) => {
+			let answer = document.querySelector("#poll__answers");
+			let code = `<button class="poll__answer">
+${pollAnsers[index]}
+</button>`
+			answer.insertAdjacentHTML('beforeend', code);
+		})
+	}
+	// КНОПКА
+	let btn = document.querySelectorAll('button');
+	btn.forEach(item => {
+		item.addEventListener("click", e => {
+			if (item) {
+				alert('Спасибо, ваш голос засчитан!')
+				location.reload(true)
+			}
+		})
+	})
+}
+xhr.send();
