80b477bd3bd373dc58b2a0dce5116385fe5045b7
Alex hitsaaja <sto98@inbox.ru>
2024-05-08 12:05:38 +0300

    commit 0805241305 task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,16 +3,7 @@ let input = document.querySelector("#task__input");
 let btn = document.querySelector("#tasks__add");
 let result = document.querySelector("#tasks__list");
 
-// кнопка добавить новую задачу
-btn.addEventListener("click", (e) => {
-	e.preventDefault();
-	createElements(input.value);
-	input.value = ''.trim();
-})
-
-// поле новая задача
-function createElements(value) {
-	if (input.value === ''.trim()) return;
+function todoListFiled() {
 	let divTag = `<div class="task">
                       <div class="task__title">
                         ${input.value}
@@ -20,13 +11,22 @@ function createElements(value) {
                       <a href="#" class="task__remove">&times;</a>
                     </div>`
 	result.insertAdjacentHTML('beforeend', divTag);
-	
-// удаление поля новая задача
-	window.addEventListener("click", e => {
-		e.preventDefault();
-		if (e.target.classList.contains('task__remove')) {
-			const card = e.target.closest(".task");
-			card.remove();
-		};
-	});
 };
+
+btn.addEventListener("click", e => {
+	e.preventDefault();
+	if (input.value.trim().length === 0) {
+		return
+	} else {
+		todoListFiled();
+		input.value = null;
+	};
+});
+
+result.addEventListener("click", e => {
+	e.preventDefault();
+	if (e.target.classList.contains('task__remove')) {
+		const card = e.target.closest(".task");
+		card.remove();
+	};
+});
