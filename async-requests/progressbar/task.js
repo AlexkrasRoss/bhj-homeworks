f93289b31d6622175c6d4c8d6abfa125071ad33a
Alex hitsaaja <sto98@inbox.ru>
2024-06-12 23:58:00 +0300

    commit 1206242357 task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,17 +1,25 @@
 const form = document.getElementById('form');
-const reURL = 'https://students.netoservices.ru/nestjs-backend/upload';
-const progress = document.getElementById('progress');
-form.addEventListener('submit', (e) => {
+const btn = document.getElementById('send');
+const fileInput = document.getElementById("file");
+const progress = document.getElementById("progress");
+
+form.addEventListener('submit', e => {
 	let formData = new FormData(form);
-	const xhr = new XMLHttpRequest();
-	xhr.open('POST', reURL);
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+	xhr.upload.onprogress = (e) => {
+		progress.value = Math.round(e.loaded / e.total);
+	}
 	xhr.addEventListener('readystatechange', function() {
-		if (xhr.readyState === xhr.DONE) {
-			progress.value = 1;
+		if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+			let file = fileInput.files[0];
+			formData.get(file);
 		} else {
-			progress.value = progress.value + 0.1;
+			xhr.upload.onerror = function() {
+				alert('Произошла ошибка при загрузке данных на сервер!')
+			}
 		}
 	})
-	xhr.send(formData)
+	xhr.send(formData);
 	e.preventDefault();
 })
