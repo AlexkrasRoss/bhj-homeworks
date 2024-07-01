13e4646fb5ce58e1577f0d12223c342a3cf895bb
Alex hitsaaja <sto98@inbox.ru>
2024-07-02 00:15:48 +0300

    file change 2 task.js

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,15 +1,17 @@
 let texTarea = document.querySelector('#editor');
 let btn = document.querySelector('.btn');
+
 // сохраняем текст в  localStorage
-texTarea.addEventListener('keydown', function(e){
-   localStorage.setItem('text', texTarea.value);
-  });
+texTarea.addEventListener('keydown', function(e) {
+	localStorage.setItem('text', texTarea.value);
+});
 
 // получаем текст из  localStorage и выводим в текстовое поле
-  texTarea.innerText=localStorage.getItem('text');
- // очищаем текстовое поле и localStorage
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
-    localStorage.removeItem('text');
-    texTarea.value =''; 
- });
+texTarea.innerText = localStorage.getItem('text');
+
+// очищаем текстовое поле и localStorage
+btn.addEventListener("click", function(e) {
+	e.preventDefault();
+	localStorage.removeItem('text');
+	texTarea.value = '';
+});
