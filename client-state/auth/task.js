f8afbd9d37f78f945d094ec1c1575aee9c4a9a3f
Alex hitsaaja <sto98@inbox.ru>
2024-07-04 16:56:52 +0300

    file change 1 task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -8,31 +8,31 @@ const password = document.querySelector('[name="password"]');
 const xhr = new XMLHttpRequest();
 xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
 xhr.responseType = 'json';
-
+if (localStorage.getItem('password') !== null) {
+	form.remove();
+	welcome.classList.add("welcome_active");
+	userId.innerHTML = localStorage.getItem('password');
+}; 
 form.addEventListener('submit', e => {
 	e.preventDefault();
-
 	if (login.value === '' || password.value === '') {
-		alert('Поле не заполнено')
+		alert('Поле не заполнено');
+		form.reset();
 		return
-	}
-
+	};
 	const formData = new FormData(form);
 	xhr.onload = () => {
 		const result = xhr.response;
 		userId.innerHTML = result.user_id;
-
 		if (result.success === false) {
-			alert('Неверный логин/пароль')
-			login.value = ''
-			password.value = ''
-			return
-		} else {
-			form.remove()
-			welcome.classList.add("welcome_active");
-			localStorage.setItem('login', form.login.value);
-			localStorage.setItem('password', form.password.value);
-		}
-	}
+			alert('Неверный логин/пароль');
+			form.reset();
+			return;
+		};
+		form.remove();
+		welcome.classList.add("welcome_active");
+		localStorage.setItem('login', form.login.value);
+		localStorage.setItem('password', result.user_id);
+	};
 	xhr.send(formData);
 });
