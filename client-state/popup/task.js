cf9e8b1cc14b0b48b2e405d17715dea587f89464
Alex hitsaaja <sto98@inbox.ru>
2024-07-04 17:01:41 +0300

    file change 1 task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,13 +1,13 @@
 const modal = document.querySelector('.modal');
 let modalClose = document.querySelector('.modal__close');
 
-if (document.cookie) {
+if (document.cookie.includes('popup') === true) {
 	modal.classList.remove('modal_active')
 } else {
 	modal.classList.add('modal_active')
 }
 
 modalClose.addEventListener('click', function() {
-	document.cookie = "user=Alex; path=/; ";
+	document.cookie = "popup=popupClosed; path=/; ";
 	modal.classList.remove('modal_active')
 })
